1daea343a699a105161da42c08a65640bb3f60fd
Alex Caradonna <alex-caradonna@idexx.com>
2021-05-02 22:43:24 -0400

    add module for node; add js directory; add config.js containing configuration for aws cognito

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: ' us-east-1_zKCB0NJ5T', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4f8c0885fdrftr2flvtmnephgh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
